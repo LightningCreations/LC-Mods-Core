f543505c2ffc44be30e8a1a29f940781ce59ad12
Connor Horman <chorman64@gmail.com>
2019-05-12 12:19:00 -0400

    Move byName comparison to Comparators

--- src/main/java/github/chorman0773/gac14/util/Comparators.java
+++ src/main/java/github/chorman0773/gac14/util/Comparators.java
@@ -3,8 +3,13 @@ package github.chorman0773.gac14.util;
 import java.util.Comparator;
 import java.util.function.Function;
 
+import net.minecraft.util.ResourceLocation;
+
 public interface Comparators {
-	public static <T,U> Comparator<U> with(Comparator<? super T> comp,Function<? super U,? extends T> func){
-		return (a,b)->comp.compare(func.apply(a), func.apply(b));
+	static final Comparator<ResourceLocation> stringOrder = Comparators.with(String.CASE_INSENSITIVE_ORDER, ResourceLocation::toString);
+
+	static <T,U> Comparator<T> with(Comparator<? super U> comparator, Function<? super T,? extends U> func) {
+		// TODO Auto-generated method stub
+		return Comparator.comparing(func, comparator);
 	}
 }
